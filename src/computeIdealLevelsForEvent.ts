e4c546fb4c03f8e000f4256d0a62cf35e60b09b7
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-04 18:00:06 -0500

    introduce concept of workshop

--- src/computeIdealLevelsForEvent.ts
+++ src/computeIdealLevelsForEvent.ts
@@ -1,11 +1,26 @@
 import { importProducts } from "./importProducts";
 import { maxLevelShouldBe } from "./maxLevelShouldBe";
+import { Workshop } from "./types/Workshop";
 
 export function computeIdealLevelsForEvent() {
     const eventName: string = "From Dust Till Lawn";
-    const products = importProducts(eventName.replace(/\s/g,""));
+    const products: Product[] = importProducts(eventName.replace(/\s/g,""));
+    const workshop: Workshop = setUpWorkshop(products);
     const maxLvlProd1 = maxLevelShouldBe(products[1].buildCost, products[0]);
     return maxLvlProd1;
 }
 
-console.log(computeIdealLevelsForEvent());
+function setUpWorkshop(products: Product[]): Workshop {
+    const levels = new Map<string, number>();
+    const merchants = new Map<string, number>();
+    for(let product of products) {
+        levels.set(product.name, 0);
+        merchants.set(product.name, 0);
+    }
+
+    return {
+        products,
+        productLevels: levels,
+        productMerchants: merchants,
+    }
+}
