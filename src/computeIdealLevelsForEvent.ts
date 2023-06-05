1eb4694bee102325085d6f946336e90cc43959e4
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-05 13:21:41 -0500

    add eslint

--- src/computeIdealLevelsForEvent.ts
+++ src/computeIdealLevelsForEvent.ts
@@ -1,33 +1,33 @@
-import { importProducts } from "./importProducts";
-import { maxLevelShouldBe } from "./maxLevelShouldBe";
-import { ProductStatus, Workshop } from "./types/Workshop";
+import { importProducts } from './importProducts';
+import { maxLevelShouldBe } from './maxLevelShouldBe';
+import { type ProductStatus, type Workshop } from './types/Workshop';
 
-export function computeIdealLevelsForEvent() {
-    const eventName: string = "From Dust Till Lawn";
-    const products: Map<string, Product> = importProducts(eventName.replace(/\s/g,""));
-    const workshop: Workshop = setUpWorkshop(products);
-    const productsInOrder = Array.from(products.keys());
-    const maxLvlProd1 = maxLevelShouldBe(
-        products.get(productsInOrder[1]).buildCost,
-        products.get(productsInOrder[0]),
-        workshop
-    );
-    return maxLvlProd1;
+export function computeIdealLevelsForEvent (): number {
+  const eventName: string = 'From Dust Till Lawn';
+  const products: Map<string, Product> = importProducts(eventName.replace(/\s/g, ''));
+  const workshop: Workshop = setUpWorkshop(products);
+  const productsInOrder = Array.from(products.keys());
+  const maxLvlProd1 = maxLevelShouldBe(
+    products.get(productsInOrder[1]).buildCost,
+    products.get(productsInOrder[0]),
+    workshop,
+  );
+  return maxLvlProd1;
 }
 
-function setUpWorkshop(products: Map<string, Product>): Workshop {
-    const statuses = new Map<string, ProductStatus>();
-    const status: ProductStatus = {
-        level: 0,
-        merchants: 0,
-    };
+function setUpWorkshop (products: Map<string, Product>): Workshop {
+  const statuses = new Map<string, ProductStatus>();
+  const status: ProductStatus = {
+    level: 0,
+    merchants: 0,
+  };
 
-    for(const productName of products.keys()) {
-        statuses.set(productName, status);
-    }
+  for (const productName of products.keys()) {
+    statuses.set(productName, status);
+  }
 
-    return {
-        products,
-        statuses,
-    }
+  return {
+    products,
+    statuses,
+  };
 }
--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,14 +1,14 @@
-type Product = {
-    name: string;
-    researchCost: number;
-    buildCost: number;
-    revenue: number;
-    outputCount: number;
-    input1: InputProduct | null;
-    input2: InputProduct | null;
-}
+interface Product {
+  name: string
+  researchCost: number
+  buildCost: number
+  revenue: number
+  outputCount: number
+  input1: InputProduct | null
+  input2: InputProduct | null
+};
 
-type InputProduct = {
-    product: Product,
-    count: number;
+interface InputProduct {
+  product: Product
+  count: number
 }
