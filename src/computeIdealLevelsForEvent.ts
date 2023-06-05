668b9d811f30956b7c31049f8e40991f3bd105ff
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-05 13:56:28 -0500

    refactor importing inputs, fix paren spaces

--- src/computeIdealLevelsForEvent.ts
+++ src/computeIdealLevelsForEvent.ts
@@ -2,20 +2,22 @@ import { importProducts } from './importProducts';
 import { maxLevelShouldBe } from './maxLevelShouldBe';
 import { type ProductStatus, type Workshop } from './types/Workshop';
 
-export function computeIdealLevelsForEvent (): number {
+export function computeIdealLevelsForEvent(): number {
   const eventName: string = 'From Dust Till Lawn';
   const products: Map<string, Product> = importProducts(eventName.replace(/\s/g, ''));
   const workshop: Workshop = setUpWorkshop(products);
   const productsInOrder = Array.from(products.keys());
   const maxLvlProd1 = maxLevelShouldBe(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     products.get(productsInOrder[1])!.buildCost,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     products.get(productsInOrder[0])!,
     workshop,
   );
   return maxLvlProd1;
 }
 
-function setUpWorkshop (products: Map<string, Product>): Workshop {
+function setUpWorkshop(products: Map<string, Product>): Workshop {
   const statuses = new Map<string, ProductStatus>();
   const status: ProductStatus = {
     level: 0,
