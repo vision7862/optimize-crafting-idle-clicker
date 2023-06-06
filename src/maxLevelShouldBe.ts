668e9793ba839d1f88ab263103ee3ee0b438bc8b
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-06 18:18:35 -0500

    add function to calculate fastest way up to target amount

--- src/maxLevelShouldBe.ts
+++ src/maxLevelShouldBe.ts
@@ -45,3 +45,27 @@ export function optimizeLevelsBelowProduct(
   } while (shouldUpgradeNext);
   return modifiedWorkshop;
 }
+
+export function optimizeAllLevelsToTarget(
+  target: number,
+  workshop: Workshop,
+): Workshop {
+  let modifiedWorkshop = workshop;
+  const productsInOrder = Array.from(workshop.products.keys());
+  for (let productIndex = 0; productIndex < productsInOrder.length; productIndex++) {
+    const thisProduct: Product | undefined = workshop.products.get(productsInOrder[productIndex]);
+    const nextProduct: Product | undefined = workshop.products.get(productsInOrder[productIndex + 1]);
+    if (thisProduct !== undefined) {
+      const currentTarget = nextProduct !== undefined ? Math.min(target, nextProduct.buildCost) : target;
+      const upgradedWorkshop: Workshop | null = optimizeLevelsBelowProduct(currentTarget, thisProduct, modifiedWorkshop);
+      if (upgradedWorkshop != null) {
+        modifiedWorkshop = upgradedWorkshop;
+      }
+      if (nextProduct === undefined || target < nextProduct.buildCost) {
+        break;
+      }
+    }
+  }
+
+  return modifiedWorkshop;
+}
