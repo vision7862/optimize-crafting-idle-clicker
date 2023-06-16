577f83fff89120c8271fe871df63dd29cecd5a14
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-16 14:43:31 -0500

    add WorkshopStatus for event/level/scientists

--- src/types/Workshop.ts
+++ src/types/Workshop.ts
@@ -1,9 +1,13 @@
-export interface Workshop {
+export type Workshop = Readonly<{
   products: Map<string, Product>
   statuses: Map<string, ProductStatus>
+}> & WorkshopStatus;
+
+export type WorkshopStatus = Readonly<{
   event: boolean
   level: number
-};
+  scientists: number
+}>;
 
 export interface ProductStatus {
   level: number
