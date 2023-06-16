f8e795d69916d3af211f6986d559eaab5aa25330
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-06-16 17:25:53 -0500

    refactor: introduce getProductByName

--- src/WorkshopHelpers.ts
+++ src/WorkshopHelpers.ts
@@ -1,4 +1,4 @@
-import { type ProductStatus, type Workshop } from './types/Workshop';
+import { type Product, type ProductStatus, type Workshop } from './types/Workshop';
 
 export function getStatusMap(workshop: Workshop): Map<string, ProductStatus> {
   const statuses = new Map<string, ProductStatus>();
@@ -6,4 +6,13 @@ export function getStatusMap(workshop: Workshop): Map<string, ProductStatus> {
     statuses.set(product.details.name, product.status);
   }
   return statuses;
+}
+
+export function getProductByName(productName: string, productsInfo: Map<string, Product>): Product {
+  const product: Product | undefined = productsInfo.get(productName);
+  if (product !== undefined) {
+    return product;
+  } else {
+    throw new Error('Workshop does not contain ' + productName);
+  }
 }
