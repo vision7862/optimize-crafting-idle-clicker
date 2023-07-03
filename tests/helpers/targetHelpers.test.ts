bb60f0260f24f507292ad852b715399051218c18
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-07-03 13:08:39 -0500

    refactor: move research tests into their own file to mirror research helpers

--- tests/helpers/targetHelpers.test.ts
+++ tests/helpers/targetHelpers.test.ts
@@ -1,11 +1,7 @@
-import { bottomUpToMoney } from '../../src/computeIdealLevelsForEvent';
-import {
-  computeResearchTimeForWorkshop,
-  getCostOfScientists,
-  getResearchPerSecond,
-} from '../../src/helpers/ResearchHelpers';
 import { computeBuildTimeForWorkshop, computeTargetFromFame } from '../../src/helpers/targetHelpers';
-import { DEFAULT_WORKSHOP_STATUS_MAIN, Workshop, WorkshopStatus } from '../../src/types/Workshop';
+import { importMainWorkshop } from '../../src/importMainWorkshop';
+import { Product, ProductDetails, ProductStatus } from '../../src/types/Product';
+import { Workshop } from '../../src/types/Workshop';
 
 describe('targetHelpers', () => {
   describe('computeTargetFromFame', () => {
@@ -15,54 +11,6 @@ describe('targetHelpers', () => {
 
     it('should return correctly', () => {
       expect(computeTargetFromFame(1, 4)).toBe(10000);
-    });
-  });
-
-  describe('getCostOfScientists', () => {
-    it('should return x for y', () => {
-      expect(getCostOfScientists(1)).toBe(0);
-    });
-
-    it('should return x for y', () => {
-      expect(getCostOfScientists(2)).toBe(50);
-    });
-
-    it('should return x for y', () => {
-      expect(getCostOfScientists(3)).toBe(107);
-    });
-
-    it('should return x for y', () => {
-      expect(getCostOfScientists(4)).toBe(173);
-    });
-  });
-
-  describe('computeResearchTimeForWorkshop', () => {
-    it.skip('should calculate research time in seconds', () => {
-      // crop main workshop after coal because manually timed about 2.5 minutes
-      const workshop = bottomUpToMoney(computeTargetFromFame(12, 21), { scientists: 520, level: 21 }).workshop;
-      const researchTime = computeResearchTimeForWorkshop(workshop);
-      expect(researchTime).toBeGreaterThan(150);
-      expect(researchTime).toBeLessThan(180);
-    });
-  });
-
-  describe('getResearchPerSecond', () => {
-    it('should account for no boost running', () => {
-      const workshopStatus: WorkshopStatus = {
-        ...DEFAULT_WORKSHOP_STATUS_MAIN,
-        scientists: 422,
-        researchBoostActive: false,
-      };
-      expect(getResearchPerSecond(workshopStatus)).toBe(5143);
-    });
-
-    it('should account for boost running', () => {
-      const workshopStatus: WorkshopStatus = {
-        ...DEFAULT_WORKSHOP_STATUS_MAIN,
-        scientists: 422,
-        researchBoostActive: true,
-      };
-      expect(getResearchPerSecond(workshopStatus)).toBe(51431);
     });
   });
 
