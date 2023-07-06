2ff4a0cf6509da4f23e37d8460776ef342abc6d6
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-07-06 14:56:19 -0500

    rework blueprint set achievement boundaries formar

--- src/helpers/blueprintScoreHelpers.ts
+++ src/helpers/blueprintScoreHelpers.ts
@@ -1,25 +1,18 @@
-import { BLUEPRINT_SETS, BlueprintSet } from '../constants/BlueprintSets';
+import { BLUEPRINT_SETS, BlueprintSet, SetMultiplierType } from '../constants/BlueprintSets';
 
 export function getIncomeMultiplierFromSets(blueprintScores: Map<string, number>): number {
   let multiplier = 1;
-  multiplier *= getMultiplierForSet('Wood', blueprintScores);
+  BLUEPRINT_SETS.filter((set: BlueprintSet) => set.multiplierType === SetMultiplierType.Income).forEach(
+    (set: BlueprintSet) => {
+      multiplier *= getMultiplierForSet(set, blueprintScores);
+    },
+  );
   return multiplier;
 }
 
-export function getMultiplierForSet(setName: string, blueprintScores: Map<string, number>): number {
-  const set: BlueprintSet = getBlueprintSet(setName);
+export function getMultiplierForSet(set: BlueprintSet, blueprintScores: Map<string, number>): number {
   const setScore = getSetBlueprintScore(set.blueprints, blueprintScores);
   return getSetAchievementMultiplier(set, setScore);
-}
-
-function getBlueprintSet(setName: string): BlueprintSet {
-  const setsMatchingName = BLUEPRINT_SETS.filter((set: BlueprintSet) => {
-    return set.setName === setName;
-  });
-  if (setsMatchingName.length !== 1) {
-    throw new Error(`Set ${setName} does not have exactly 1 match. It has ${setsMatchingName.length}.`);
-  }
-  return setsMatchingName[0];
 }
 
 export function getSetBlueprintScore(blueprints: string[], blueprintScores: Map<string, number>): number {
@@ -30,28 +23,20 @@ export function getSetBlueprintScore(blueprints: string[], blueprintScores: Map<
     summedScores += score;
     lowestScore = Math.min(lowestScore, score);
   });
-  return Math.round(summedScores * Math.log10(lowestScore > 0 ? lowestScore : 10));
+  return Math.round(summedScores * Math.log10(Math.max(lowestScore, 10)));
 }
 
 export function getSetAchievementMultiplier(set: BlueprintSet, score: number): number {
-  if (set.scoreBoundaries === undefined) {
-    throw new Error(`Set ${set.setName} does not have score boundaries`);
+  if (set.achievementRanks === undefined) {
+    console.error(`Set ${set.setName} does not have score boundaries`);
+    return 1;
   }
   let multiplier = 1;
-  const multiplierAmountPerRank: number | null | undefined = set.multiplierAmountPerRank;
-  if (multiplierAmountPerRank == null) {
-    throw new Error(`Cannot calculate achievement multiplier for set ${set.setName} by a consistent amount per rank`);
-  }
-  for (let i = 0; i < set.scoreBoundaries.length; i++) {
-    if (score < set.scoreBoundaries[i]) {
+  for (let i = 0; i < set.achievementRanks.length; i++) {
+    if (score < set.achievementRanks[i].scoreBoundary) {
       return multiplier;
     }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    multiplier += set.multiplierAmountPerRank!;
+    multiplier = set.achievementRanks[i].totalMultiplier;
   }
   return Math.round(multiplier);
 }
-
-export const BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS = {
-  getBlueprintSet,
-};
--- tests/helpers/blueprintScoreHelpers.test.ts
+++ tests/helpers/blueprintScoreHelpers.test.ts
@@ -1,38 +1,59 @@
+import { BLUEPRINT_SETS, BlueprintSet } from '../../src/constants/BlueprintSets';
 import {
-  BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS,
+  getIncomeMultiplierFromSets,
   getSetAchievementMultiplier,
   getSetBlueprintScore,
 } from '../../src/helpers/blueprintScoreHelpers';
 
 describe('blueprintScoreHelpers', () => {
+  function getBlueprintSet(setName: string): BlueprintSet {
+    const setsMatchingName = BLUEPRINT_SETS.filter((set: BlueprintSet) => {
+      return set.setName === setName;
+    });
+    if (setsMatchingName.length !== 1) {
+      throw new Error(`Set ${setName} does not have exactly 1 match. It has ${setsMatchingName.length}.`);
+    }
+    return setsMatchingName[0];
+  }
+
+  describe('getIncomeMultiplierFromSets', () => {
+    it('should calculate the overall income multiplier correctly', () => {
+      const blueprintScores = new Map<string, number>([
+        ['Wood', 120],
+        ['Rawhide', 120],
+      ]);
+      expect(getIncomeMultiplierFromSets(blueprintScores)).toBe(1.2 * 1.2);
+    });
+  });
+
   describe('getSetBlueprintScore', () => {
     it('should calculate a blueprint set score correctly', () => {
       const blueprintScores = new Map<string, number>([
         ['Sickle', 64],
         ['Magnificent Dagger', 50],
       ]);
-      const blueprints = BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS.getBlueprintSet('Knife').blueprints;
+      const blueprints = getBlueprintSet('Knife').blueprints;
       expect(getSetBlueprintScore(blueprints, blueprintScores)).toBe(194);
     });
 
     it('should count missing blueprints as 10 for the log part and 0 for the summing part', () => {
       const blueprintScores = new Map<string, number>([['Sickle', 64]]);
-      const blueprints = BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS.getBlueprintSet('Knife').blueprints;
+      const blueprints = getBlueprintSet('Knife').blueprints;
       expect(getSetBlueprintScore(blueprints, blueprintScores)).toBe(64);
     });
   });
 
-  describe('getWoodSetAchievementMultiplier', () => {
+  describe('getSetAchievementMultiplier', () => {
     it('should return 2.4 if the score is between 4350 and 7500', () => {
-      expect(getSetAchievementMultiplier(BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS.getBlueprintSet('Wood'), 5000)).toBe(2.4);
+      expect(getSetAchievementMultiplier(getBlueprintSet('Wood'), 5000)).toBe(2.4);
     });
 
     it('should return 1.4 if the score is 270', () => {
-      expect(getSetAchievementMultiplier(BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS.getBlueprintSet('Wood'), 270)).toBe(1.4);
+      expect(getSetAchievementMultiplier(getBlueprintSet('Wood'), 270)).toBe(1.4);
     });
 
     it('should return 6 if the score is very high', () => {
-      expect(getSetAchievementMultiplier(BLUEPRINT_SCORE_HELPERS_TEST_EXPORTS.getBlueprintSet('Wood'), 50e6)).toBe(6);
+      expect(getSetAchievementMultiplier(getBlueprintSet('Wood'), 50e6)).toBe(6);
     });
   });
 });
