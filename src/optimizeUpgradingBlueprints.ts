cbda546a45f4efeb470ddc289707e7ce349df761
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-07-07 00:31:22 -0500

    first shot at optimizing which blueprint to upgrade

--- src/optimizeUpgradingBlueprints.ts
+++ src/optimizeUpgradingBlueprints.ts
@@ -1,20 +1,29 @@
 import { BLUEPRINT_LIBRARY } from './config/BlueprintLibrary';
 import { SetMultiplierType } from './constants/BlueprintSets';
-import { convertBlueprintLibraryToScores, getSpecifiedMultiplierFromSets } from './helpers/blueprintScoreHelpers';
-import { getCostToUpgradeBlueprint } from './helpers/blueprintUpgradeHelpers';
+import { getSpecifiedMultiplierFromLibrary } from './helpers/blueprintScoreHelpers';
+import { upgradeBlueprint } from './helpers/blueprintUpgradeHelpers';
 import { Blueprint } from './types/Blueprint';
 
 export function getBestBlueprintToUpgrade(): BlueprintUpgradeInfo | null {
-  const currentScores: Map<string, number> = convertBlueprintLibraryToScores(BLUEPRINT_LIBRARY);
-  const currentIncomeMultiplier = getSpecifiedMultiplierFromSets(SetMultiplierType.Income, currentScores);
-  let bestImprovement: number = 0;
-  let costOfBestImprovement: number = 0;
-  let bestBlueprintToUpgrade: Blueprint = undefined;
-  BLUEPRINT_LIBRARY.forEach((blueprint: Blueprint) => {
-    cost costOfUpgrading = getCostToUpgradeBlueprint(blueprint.productName);
+  const startingIncomeMultiplier = getSpecifiedMultiplierFromLibrary(SetMultiplierType.Income, BLUEPRINT_LIBRARY);
+  const inProgressLibrary = Array.from(BLUEPRINT_LIBRARY);
 
-  });
-  return { blueprint: bestBlueprintToUpgrade, costOfUpgrade: costOfBestImprovement };
+  let bestImprovement: number = 0;
+  let bestUpgrade: BlueprintUpgradeInfo | null = null;
+  for (let i = 0; i < BLUEPRINT_LIBRARY.length; i++) {
+    const blueprintToUpgrade: Blueprint = inProgressLibrary.splice(i, 1)[0];
+    const blueprintUpgradeInfo = upgradeBlueprint(blueprintToUpgrade);
+
+    inProgressLibrary.splice(i, 0, blueprintUpgradeInfo.blueprint);
+    const newIncomeMultiplier = getSpecifiedMultiplierFromLibrary(SetMultiplierType.Income, inProgressLibrary);
+    const improvement = newIncomeMultiplier / startingIncomeMultiplier / blueprintUpgradeInfo.costOfUpgrade;
+    if (improvement > bestImprovement) {
+      bestImprovement = improvement;
+      bestUpgrade = blueprintUpgradeInfo;
+    }
+  }
+
+  return bestUpgrade;
 }
 
 export type BlueprintUpgradeInfo = Readonly<{
