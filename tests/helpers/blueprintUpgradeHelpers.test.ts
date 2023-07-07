45b56521060dbac622ac945d722746c3b27af2e2
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-07-07 12:05:07 -0500

    calculate score increase by evolution stage based on 50 + 10 strategy, level by arbitrary number

--- tests/helpers/blueprintUpgradeHelpers.test.ts
+++ tests/helpers/blueprintUpgradeHelpers.test.ts
@@ -1,5 +1,5 @@
 import { DEFAULT_BLUEPRINT, DEFAULT_STAGE_2 } from '../../src/config/BlueprintLibrary';
-import { getCostToUpgradeBlueprint } from '../../src/helpers/blueprintUpgradeHelpers';
+import { getCostToUpgradeBlueprint, upgradeBlueprint } from '../../src/helpers/blueprintUpgradeHelpers';
 import { Blueprint } from '../../src/types/Blueprint';
 
 describe('blueprintUpgradeHelpers', () => {
@@ -10,7 +10,7 @@ describe('blueprintUpgradeHelpers', () => {
         productName: 'Wood',
       };
 
-      expect(getCostToUpgradeBlueprint(blueprint)).toBe(159);
+      expect(getCostToUpgradeBlueprint(blueprint, 10)).toBe(159);
     });
 
     it('should add up from not the base correctly', () => {
@@ -21,7 +21,7 @@ describe('blueprintUpgradeHelpers', () => {
         score: 20,
       };
 
-      expect(getCostToUpgradeBlueprint(blueprint)).toBe(337);
+      expect(getCostToUpgradeBlueprint(blueprint, 10)).toBe(337);
     });
 
     it('should still work with later blueprints', () => {
@@ -30,7 +30,16 @@ describe('blueprintUpgradeHelpers', () => {
         productName: 'Cut Sapphire',
       };
 
-      expect(getCostToUpgradeBlueprint(blueprint)).toBe(1267);
+      expect(getCostToUpgradeBlueprint(blueprint, 10)).toBe(1267);
+    });
+
+    it('should work for any number of levels', () => {
+      const blueprint: Blueprint = {
+        ...DEFAULT_BLUEPRINT,
+        productName: 'Cut Sapphire',
+      };
+
+      expect(getCostToUpgradeBlueprint(blueprint, 50)).toBe(50197);
     });
 
     it('should still work with non-resource products', () => {
@@ -39,7 +48,7 @@ describe('blueprintUpgradeHelpers', () => {
         productName: 'Iron Armor',
       };
 
-      expect(getCostToUpgradeBlueprint(blueprint)).toBe(564);
+      expect(getCostToUpgradeBlueprint(blueprint, 10)).toBe(564);
     });
 
     it('should use level with no regard to evolution stage or score', () => {
@@ -50,7 +59,29 @@ describe('blueprintUpgradeHelpers', () => {
         score: 120,
       };
 
-      expect(getCostToUpgradeBlueprint(blueprint)).toBe(274);
+      expect(getCostToUpgradeBlueprint(blueprint, 10)).toBe(274);
+    });
+  });
+
+  describe('upgradeBlueprint', () => {
+    it('should get the cost and score correctly for a stage 1 bp', () => {
+      const blueprint: Blueprint = {
+        ...DEFAULT_BLUEPRINT,
+        productName: 'Wood',
+      };
+      expect(upgradeBlueprint(blueprint, 10).blueprint.upgradeLevel).toBe(11);
+      expect(upgradeBlueprint(blueprint, 10).blueprint.score).toBe(20);
+    });
+
+    it('should get the cost and score correctly for a stage 2 bp', () => {
+      const blueprint: Blueprint = {
+        ...DEFAULT_STAGE_2,
+        productName: 'Copper Ingots',
+        upgradeLevel: 1,
+        score: 120,
+      };
+      expect(upgradeBlueprint(blueprint, 10).blueprint.upgradeLevel).toBe(11);
+      expect(upgradeBlueprint(blueprint, 10).blueprint.score).toBe(240);
     });
   });
 });
