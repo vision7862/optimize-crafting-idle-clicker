b3cbc3172250a0193c125f86bfd6bf52747ee3b8
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-07-16 22:34:38 -0500

    add more blueprint set optimization methods

--- src/optimizeUpgradingBlueprints.ts
+++ src/optimizeUpgradingBlueprints.ts
@@ -15,42 +15,7 @@ export type BlueprintUpgradeInfo = Readonly<{
   scoreChange: number;
 }>;
 
-export function getBestBlueprintToUpgrade(library: Blueprint[]): BlueprintUpgradeInfo | null {
-  const startingIncomeMultiplier = getSpecifiedMultiplierFromLibrary(SetMultiplierType.Income, library);
-
-  let bestImprovement: number = 0;
-  let bestUpgrade: BlueprintUpgradeInfo | null = null;
-  let hasBeenImproved = false;
-  let levelsToUpgrade = 10;
-  while (!hasBeenImproved && levelsToUpgrade <= 90) {
-    for (let i = 0; i < library.length; i++) {
-      // TODO: only upgrade top scoring blueprints
-      const blueprintToUpgrade: Blueprint = library.splice(i, 1)[0];
-      const topUpgradeLevel = 51 + (blueprintToUpgrade.evolutionStage - 1) * 10;
-      if (blueprintToUpgrade.upgradeLevel >= topUpgradeLevel) {
-        library.splice(i, 0, blueprintToUpgrade);
-        continue;
-      }
-
-      const blueprintUpgradeInfo = upgradeBlueprint(blueprintToUpgrade, levelsToUpgrade);
-
-      const inProgressLibrary = Array.from(library);
-      inProgressLibrary.splice(i, 0, blueprintUpgradeInfo.blueprint);
-      const newIncomeMultiplier = getSpecifiedMultiplierFromLibrary(SetMultiplierType.Income, inProgressLibrary);
-      const improvement = newIncomeMultiplier / startingIncomeMultiplier / blueprintUpgradeInfo.costOfUpgrade;
-      if (improvement > bestImprovement) {
-        hasBeenImproved = hasBeenImproved || newIncomeMultiplier > startingIncomeMultiplier;
-        bestImprovement = improvement;
-        bestUpgrade = blueprintUpgradeInfo;
-      }
-    }
-    levelsToUpgrade += 10;
-  }
-
-  return hasBeenImproved ? bestUpgrade : null;
-}
-
-export function upgradeMostImpactfulSet(blueprints: Blueprint[] = BLUEPRINT_LIBRARY): SetUpgradeInfo | null {
+export function upgradeMostImpactfulIncomeSet(blueprints: Blueprint[] = BLUEPRINT_LIBRARY): SetUpgradeInfo | null {
   let bestROI = 0;
   let bestUpgradeInfo: SetUpgradeInfo | null = null;
   let setName = '';
@@ -72,7 +37,43 @@ export function upgradeMostImpactfulSet(blueprints: Blueprint[] = BLUEPRINT_LIBR
   console.log(`to get to next rank of ${setName}`);
   return bestUpgradeInfo;
 }
+export function upgradeMostImpactfulResearchSet(blueprints: Blueprint[] = BLUEPRINT_LIBRARY): SetUpgradeInfo | null {
+  let bestROI = 0;
+  let bestUpgradeInfo: SetUpgradeInfo | null = null;
+  let setName = '';
+  BLUEPRINT_SETS.filter((set: BlueprintSet) => set.multiplierType === SetMultiplierType.Research).forEach(
+    (set: BlueprintSet) => {
+      const distanceInfo = getDistanceToNextRank(set, convertBlueprintLibraryToScores(blueprints));
+      const upgradeInfo: SetUpgradeInfo | null = upgradeSetToNextRank(set, blueprints);
+      if (upgradeInfo !== null) {
+        const roi = distanceInfo.improvement / upgradeInfo.cost;
+        if (roi > bestROI) {
+          bestROI = roi;
+          bestUpgradeInfo = upgradeInfo;
+          setName = set.setName;
+        }
+      }
+    },
+  );
+  console.log(`to get to next rank of ${setName}`);
+  return bestUpgradeInfo;
+}
 
+export function printUpgradeInfoOfEachSet(blueprints: Blueprint[] = BLUEPRINT_LIBRARY): void {
+  BLUEPRINT_SETS.forEach((set: BlueprintSet) => {
+    const distanceInfo = getDistanceToNextRank(set, convertBlueprintLibraryToScores(blueprints));
+    const upgradeInfo: SetUpgradeInfo | null = upgradeSetToNextRank(set, blueprints);
+    if (upgradeInfo !== null) {
+      const roi = distanceInfo.improvement / upgradeInfo.cost;
+      // console.log(
+      //   `upgrading ${set.setName} has an ROI of ${roi} and would cost ${upgradeInfo.cost}. it affects ${
+      //     SetMultiplierType[set.multiplierType]
+      //   }`,
+      // );
+      console.log(`${set.setName};${roi};${upgradeInfo.cost};${SetMultiplierType[set.multiplierType]}`);
+    }
+  });
+}
 export function upgradeAllBlueprintsToLoreLimit(
   lore: number,
   blueprints: Blueprint[] = BLUEPRINT_LIBRARY,
@@ -81,7 +82,7 @@ export function upgradeAllBlueprintsToLoreLimit(
   let totalCost = 0;
   let inProgressBlueprintLibrary = Array.from(blueprints);
   while (totalCost < lore) {
-    const upgradeInfo = upgradeMostImpactfulSet(inProgressBlueprintLibrary);
+    const upgradeInfo = upgradeMostImpactfulIncomeSet(inProgressBlueprintLibrary);
     if (upgradeInfo === null || totalCost + upgradeInfo.cost >= lore) {
       break;
     }
