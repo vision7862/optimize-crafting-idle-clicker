c471d20b812ff7abb61d56f98ce8f852188691d6
vision7862 <140125930+vision7862@users.noreply.github.com>
2023-07-23 14:52:54 -0500

    Add issue references to existing TODOs (#12)

--- src/constants/Achievements.ts
+++ src/constants/Achievements.ts
@@ -1,4 +1,4 @@
-// TODO: include rank information for achievements that do not come from blueprint sets (login, top income reached, etc)
+// TODO: GH-3: include rank information for achievements that do not come from blueprint sets (login, top income reached, etc)
 
 export const MWS_LOYALTY_ACHIEVE_MERCHANT_MULTIPLIER = 8.5;
 
@@ -8,3 +8,4 @@ export const MWS_EVENT_ACHIEVE_INCOME_MULTIPLIER = 6;
 
 export const DAILY_DYNASTY_FRIEND_BONUS_MERCHANT = 6;
 export const DAILY_DYNASTY_FRIEND_BONUS_ORE = 2;
+// TODO: GH-8: handle dynasty bonuses better
