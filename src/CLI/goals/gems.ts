d028eb504248637ef4891cc7a72794fc2b745506
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-08-09 10:25:04 -0500

    add event version for gems goal

--- src/CLI/goals/gems.ts
+++ src/CLI/goals/gems.ts
@@ -1,26 +1,30 @@
 import { bestGemChance } from '../../buildWorkshop/computeIdealLevelsForEvent';
 import { isEvent } from '../../buildWorkshop/helpers/WorkshopHelpers';
-import { printInfo } from '../../buildWorkshop/helpers/printResults';
+import { printFameTime, printInfo } from '../../buildWorkshop/helpers/printResults';
 import { computeTargetFromFame } from '../../buildWorkshop/helpers/targetHelpers';
 import { WorkshopStatus } from '../../buildWorkshop/types/Workshop';
 import { GoalType } from '../CLI';
 
 function optimizeForGems(workshopStatus: Partial<WorkshopStatus>): void {
-  const targetInfo = bestGemChance(workshopStatus);
-  printInfo(
-    targetInfo.upgradeInfo,
-    computeTargetFromFame(
-      targetInfo.fame,
-      targetInfo.upgradeInfo.workshop.workshopStatus.level,
-      isEvent(targetInfo.upgradeInfo.workshop.workshopStatus),
-    ),
-  );
+  if (isEvent(workshopStatus)) {
+    printFameTime(9, workshopStatus);
+  } else {
+    const targetInfo = bestGemChance(workshopStatus);
+    printInfo(
+      targetInfo.upgradeInfo,
+      computeTargetFromFame(
+        targetInfo.fame,
+        targetInfo.upgradeInfo.workshop.workshopStatus.level,
+        isEvent(targetInfo.upgradeInfo.workshop.workshopStatus),
+      ),
+    );
+  }
 }
 
 export const gemsGoal: GoalType = {
   name: 'gems',
-  description: 'most efficient time per gem percentage of 14 vs 15 fame',
-  shouldShow: (workshopStatus: WorkshopStatus) => !isEvent(workshopStatus),
+  description: 'most efficient time per gem percentage of 14 vs 15 fame, or flat 9 fame if optimizing an event',
+  shouldShow: (_workshopStatus: WorkshopStatus) => true,
   selectOption: (workshopStatus: WorkshopStatus) => {
     optimizeForGems(workshopStatus);
   },
