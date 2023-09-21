6ef09e3fe3c637acabef444c77429afeb13a2c9c
vision.7862 <140125930+vision7862@users.noreply.github.com>
2023-09-21 00:32:51 -0500

    make map instead of case statement

--- src/buildWorkshop/helpers/eventPassHelpers.ts
+++ src/buildWorkshop/helpers/eventPassHelpers.ts
@@ -1,13 +1,15 @@
 import { FREE_PASS, MINMAXER_PASS, SUPPORTER_PASS } from '../constants/EventPassMultipliers';
 import { EventPass, EventPassName } from '../types/EventPass';
 
+const passMap = new Map<EventPassName, EventPass>([
+  [EventPassName.free, FREE_PASS],
+  [EventPassName.supporter, SUPPORTER_PASS],
+  [EventPassName.minmaxer, MINMAXER_PASS],
+]);
 export function getCurrentEventPassMultipliers(name: EventPassName): EventPass {
-  switch (name) {
-    case EventPassName.free:
-      return FREE_PASS;
-    case EventPassName.supporter:
-      return SUPPORTER_PASS;
-    case EventPassName.minmaxer:
-      return MINMAXER_PASS;
+  const passMultipliers: EventPass | undefined = passMap.get(name);
+  if (passMultipliers === undefined) {
+    throw new Error(`event pass "${name}" does not exist. create a new workshop status object.`);
   }
+  return passMultipliers;
 }
